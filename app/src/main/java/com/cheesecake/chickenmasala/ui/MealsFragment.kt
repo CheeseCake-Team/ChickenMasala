93528f56339a1dca0a5bf52bac4c2902a9ff4616
ali3030 <ali3030@users.noreply.github.com>
2023-03-22 02:54:04 +0300

    create fillLinerLayoutWithCardItem method override the onCreateView method

--- app/src/main/java/com/cheesecake/chickenmasala/ui/MealsFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/MealsFragment.kt
@@ -1,9 +1,32 @@
 package com.cheesecake.chickenmasala.ui
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.cheesecake.chickenmasala.databinding.FragmentMealsBinding
+import com.cheesecake.chickenmasala.databinding.ItemMealCardBinding
 
 class MealsFragment : BaseFragment<FragmentMealsBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentMealsBinding =
         FragmentMealsBinding::inflate
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fillLinerLayoutWithCardItem()
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
+
+    fun fillLinerLayoutWithCardItem(){
+        for (i in 0 until 20) {
+            val cardItemView = ItemMealCardBinding.inflate(layoutInflater)
+
+
+            binding!!.leaner.addView(cardItemView.root)
+        }
+    }
 }
