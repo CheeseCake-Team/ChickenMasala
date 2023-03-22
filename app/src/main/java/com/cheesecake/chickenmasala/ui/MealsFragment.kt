0d2f8d6a539bc0402e6c3f029d6a6548f9fc98f9
ziad Abo Elela <ziaadaboelela@gmail.com>
2023-03-22 17:04:19 +0200

    add card of meal to fragment_meals

--- app/src/main/java/com/cheesecake/chickenmasala/ui/MealsFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/MealsFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cheesecake.chickenmasala.R
 import com.cheesecake.chickenmasala.databinding.FragmentMealsBinding
 import com.cheesecake.chickenmasala.databinding.ItemMealCardBinding
+import com.cheesecake.chickenmasala.model.Meal
+import com.squareup.picasso.Picasso
+import kotlin.time.Duration.Companion.minutes
 
-class MealsFragment : BaseFragment<FragmentMealsBinding>() {
+class MealsFragment(private val meals: List<Meal>) : BaseFragment<FragmentMealsBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentMealsBinding =
         FragmentMealsBinding::inflate
 
@@ -18,9 +22,30 @@ class MealsFragment : BaseFragment<FragmentMealsBinding>() {
     }
 
     private fun fillLinerLayoutWithCardItem() {
-        for (i in 0 until 20) {
-            val cardItemView = ItemMealCardBinding.inflate(layoutInflater)
-            binding.linear.addView(cardItemView.view2)
+        val cardItemView = ItemMealCardBinding.inflate(layoutInflater)
+
+        meals.forEach {meal ->
+            cardItemView.tvNameOfMeal.text = meal.translatedRecipeName
+            cardItemView.tvTimeToMakeMeal.text = meal.TotalTimeInMinutes.toString()
+            cardItemView.tvLocationOfMeal.text = meal.cuisine
+
+            Glide.with(this).load(meal.imageUrl).into(cardItemView.imageCardOfMeal)
+
+            cardItemView.root.setOnClickListener {
+                loadMealFragment(meal)
+            }
+
         }
+
+
+        binding.linear.addView(cardItemView.root)
+
+    }
+
+    private fun loadMealFragment(meal: Meal) {
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, MealFragment(meal))
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
