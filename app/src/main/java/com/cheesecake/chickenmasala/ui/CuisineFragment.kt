4ca78652ff88cf77e078d1e59d0b5689364271af
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-03-22 17:43:05 +0300

    implement on click listener for cuisine item add load mealFragment method add fillCuisines method

--- app/src/main/java/com/cheesecake/chickenmasala/ui/CuisineFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/CuisineFragment.kt
@@ -3,23 +3,36 @@ package com.cheesecake.chickenmasala.ui
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import com.cheesecake.chickenmasala.R
 import com.cheesecake.chickenmasala.databinding.FragmentCuisineBinding
 import com.cheesecake.chickenmasala.databinding.ItemCuisineBinding
+import com.cheesecake.chickenmasala.model.Meal
+import com.cheesecake.chickenmasala.model.Recipes
 
 
-class CuisineFragment : BaseFragment<FragmentCuisineBinding>() {
+class CuisineFragment(private val recipes: Recipes) : BaseFragment<FragmentCuisineBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentCuisineBinding =
         FragmentCuisineBinding::inflate
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fillLinearLayoutWithCard()
+        fillCuisines()
     }
 
-    private fun fillLinearLayoutWithCard() {
-        for (recipe in 0..20) {
+    private fun fillCuisines(){
+        recipes.cuisines.forEach {cuisine ->
             val itemCuisineBinding = ItemCuisineBinding.inflate(layoutInflater)
+             itemCuisineBinding.cuisineCountry.text = cuisine
+
+            itemCuisineBinding.root.setOnClickListener { loadMealsFragment(recipes.getCuisineRecipe(cuisine)) }
+
             binding.cuisineContainer.addView(itemCuisineBinding.root)
         }
+    }
+    private fun loadMealsFragment(meals: List<Meal>) {
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, MealsFragment(meals))
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
