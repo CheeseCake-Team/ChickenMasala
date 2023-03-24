52c1580ac200bf6361b06a2a0c54d24583a504f3
Mahmoud Elsayed <mahmoudelsayedessa@gamil.com>
2023-03-25 00:34:43 +0200

    add Parcelable plugin and make Meal implement Parcelable

--- app/src/main/java/com/cheesecake/chickenmasala/model/Meal.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/model/Meal.kt
@@ -1,14 +1,18 @@
 package com.cheesecake.chickenmasala.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Meal(
     val translatedRecipeName: String,
     val TotalTimeInMinutes: Int,
     val cuisine: String,
     val translatedInstructions: List<String>,
     val url: String,
-    val cleanedIngredients:List<String>,
+    val cleanedIngredients: List<String>,
     val translatedIngredients: List<String>,
     val imageUrl: String,
-    val ingredientCount:Int,
+    val ingredientCount: Int,
     val course: MealCourse?
-)
+) : Parcelable
