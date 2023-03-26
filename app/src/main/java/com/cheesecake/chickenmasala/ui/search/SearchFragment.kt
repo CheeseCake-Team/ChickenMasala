0a6c7a9e0c17096c4c32d547ad97d2f3aa00764a
ali3030 <ali3030@users.noreply.github.com>
2023-03-26 23:05:44 +0300

    edit SearchFragment logic

--- app/src/main/java/com/cheesecake/chickenmasala/ui/search/SearchFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/search/SearchFragment.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ArrayAdapter
 import com.cheesecake.chickenmasala.databinding.ChipsInjectBinding
 import com.cheesecake.chickenmasala.databinding.FragmentSearchBinding
 import com.cheesecake.chickenmasala.model.Constants
 import com.cheesecake.chickenmasala.model.Meal
 import com.cheesecake.chickenmasala.model.RecipesManager
 import com.cheesecake.chickenmasala.ui.base.BaseFragment
-import com.google.android.material.chip.Chip
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentSearchBinding =
         FragmentSearchBinding::inflate
-
 
     private var recipesManager: RecipesManager? = null
     private lateinit var searchBarInputs: MutableList<String>
@@ -23,53 +23,65 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recipesManager = arguments?.getParcelable(Constants.MAIN_ACTIVITY_RECIPES)
+        setupAutoCompleteTextView()
+    }
+
+    private fun setupAutoCompleteTextView() {
+        val suggestions: List<String> = recipesManager?.indianIngredients?.distinct()?.sorted() ?: emptyList<String>()
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            suggestions
+        )
+        binding.searchAutoCompleteTextView.setAdapter(adapter)
     }
 
 
     override fun onStart() {
         super.onStart()
-        val x = collectAllIngredients()
-        x.forEach {
-
-        }
+        collectAllIngredients()
     }
-
 
     private fun collectAllIngredients(): MutableList<String> {
         searchBarInputs = mutableListOf<String>()
         binding.searchAutoCompleteTextView.setOnKeyListener { _, keyCode, event ->
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                val x = binding.searchAutoCompleteTextView.text.toString() + "\n"
-                searchBarInputs.add(x)
-                val chip = ChipsInjectBinding.inflate(layoutInflater)
-                chip.customChip.text = x
-                binding.result.addView(chip.root)
-                binding.searchAutoCompleteTextView.setText("")
-                return@setOnKeyListener true
+                val inputText = binding.searchAutoCompleteTextView.text.toString().trim()
+                if (inputText.isNotEmpty()) {
+                    searchBarInputs.add(inputText)
+                    createChip(inputText)
+                    binding.searchAutoCompleteTextView.setText("")
+                    return@setOnKeyListener true
+                }
             }
             return@setOnKeyListener false
         }
         return searchBarInputs
     }
 
-    private fun injectChip(word:String){
+    private fun createChip(text: String) {
+        val chip = ChipsInjectBinding.inflate(layoutInflater)
+        chip.customChip.text = text
+        binding.result.addView(chip.root)
 
+        // Add a click listener to remove the chip and the corresponding ingredient from the search bar inputs.
+        chip.customChip.setOnCloseIconClickListener {
+            searchBarInputs.remove(text)
+            binding.result.removeView(chip.root)
+        }
     }
 
     private fun searchAllMealsByIngredients(): List<Meal>? {
         return recipesManager?.getIndianFoodSearch?.searchByIngredients(searchBarInputs)
             ?.getSearchedMeals()
-
     }
 
-
     companion object {
-        fun createFragment(recipesManager: RecipesManager): SearchFragment {
-            val bundle = Bundle()
-            bundle.putParcelable(Constants.MAIN_ACTIVITY_RECIPES, recipesManager)
-            val fragment = SearchFragment()
-            fragment.arguments = bundle
-            return fragment
+        fun createFragment(recipesManager: RecipesManager) = SearchFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(Constants.MAIN_ACTIVITY_RECIPES, recipesManager)
+            }
         }
     }
 }
