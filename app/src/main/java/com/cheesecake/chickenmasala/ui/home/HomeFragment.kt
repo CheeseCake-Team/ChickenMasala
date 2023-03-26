79ae0261c3a10c3faa5fe026dfc36a36a14a55da
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-03-27 00:00:13 +0300

    made home recycler view xml + logic

--- app/src/main/java/com/cheesecake/chickenmasala/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/home/HomeFragment.kt
@@ -1,30 +1,38 @@
 package com.cheesecake.chickenmasala.ui.home
 
 import android.view.LayoutInflater
-import android.widget.LinearLayout
-import com.bumptech.glide.Glide
 import com.cheesecake.chickenmasala.R
 import com.cheesecake.chickenmasala.databinding.FragmentHomeBinding
-import com.cheesecake.chickenmasala.databinding.ItemRecipesBinding
 import com.cheesecake.chickenmasala.model.Meal
 import com.cheesecake.chickenmasala.model.RecipesManager
 import com.cheesecake.chickenmasala.ui.base.BaseFragment
 import com.cheesecake.chickenmasala.ui.meal.MealFragment
 
-class HomeFragment(private val recipes: RecipesManager) : BaseFragment<FragmentHomeBinding>() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentHomeBinding =
         FragmentHomeBinding::inflate
 
     override fun onStart() {
         super.onStart()
-        val fastRecipeAdapter = HomeRecipeAdapter(HomeRecipeListener { loadMealFragment(it) })
-        val recipesOfTodayAdapter = HomeRecipeAdapter(HomeRecipeListener { loadMealFragment(it) })
-        binding.recyclerViewFastRecipes.adapter = fastRecipeAdapter
-        binding.recyclerViewRecipesOfToday.adapter = recipesOfTodayAdapter
-
-        fastRecipeAdapter.submitList(recipes.getFastMeals())
-        recipesOfTodayAdapter.submitList(recipes.getRandomMeals())
+        val fastRecipeAdapter =
+            HomeRecipeAdapter(HomeRecipeListener { loadMealFragment(it) }).apply {
+                submitList(RecipesManager.getFastMeals())
+            }
+        val recipesOfTodayAdapter =
+            HomeRecipeAdapter(HomeRecipeListener { loadMealFragment(it) }).apply {
+                submitList(RecipesManager.getRandomMeals())
+            }
+        val lowIngredientsFoodAdapter =
+            HomeRecipeAdapter(HomeRecipeListener { loadMealFragment(it) }).apply {
+                submitList(RecipesManager.getLowIngredientsMeals())
+            }
+        binding.apply {
+            recyclerViewFastRecipes.adapter = fastRecipeAdapter
+            recyclerViewRecipesOfToday.adapter = recipesOfTodayAdapter
+            recyclerViewLowIngredientsFood.adapter = lowIngredientsFoodAdapter
+        }
     }
+
 
     private fun loadMealFragment(meal: Meal) {
         val transaction = requireActivity().supportFragmentManager.beginTransaction()
