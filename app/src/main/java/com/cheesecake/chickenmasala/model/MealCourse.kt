bd89352b97214ee3ae329039c2873afd2b1aec10
Anwaar Raafat <raafatanwaar@gmail.com>
2023-03-27 00:15:28 +0200

    edit logic of category fragment and adapter

--- app/src/main/java/com/cheesecake/chickenmasala/model/MealCourse.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/model/MealCourse.kt
@@ -1,5 +1,17 @@
 package com.cheesecake.chickenmasala.model
 
-enum class MealCourse {
-    SOUPS,APPETIZER,SPICY,NORTH_INDIAN,SOUTH_INDIA,DESERT,SIDE_DISHES,DRINKS,
+import android.os.Parcelable
+import com.cheesecake.chickenmasala.R
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+enum class MealCourse( val CourseName: String, val imageResourceId: Int) : Parcelable {
+    SOUPS("Soup", R.drawable.cooking_image),
+    APPETIZER("APPETIZER", R.drawable.cooking_image),
+    SPICY("SPICY", R.drawable.cooking_image),
+    NORTH_INDIAN("NORTH_INDIAN", R.drawable.cooking_image),
+    SOUTH_INDIA("SOUTH_INDIA", R.drawable.cooking_image),
+    DESERT("DESERT", R.drawable.cooking_image),
+    SIDE_DISHES("SIDE_DISHES", R.drawable.cooking_image),
+    DRINKS("DRINKS", R.drawable.cooking_image),
 }
--- app/src/main/java/com/cheesecake/chickenmasala/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/categories/CategoriesFragment.kt
@@ -1,42 +1,46 @@
 package com.cheesecake.chickenmasala.ui.categories
 
-import android.content.Context
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import com.cheesecake.chickenmasala.R
 import com.cheesecake.chickenmasala.databinding.FragmentCategoriesBinding
-import com.cheesecake.chickenmasala.model.Meal
 import com.cheesecake.chickenmasala.ui.base.BaseFragment
-import com.cheesecake.chickenmasala.ui.meal.MealFragment
+import com.cheesecake.chickenmasala.ui.meals.MealsFragment
+import kotlinx.android.parcel.Parcelize
 
-class CategoriesFragment(private val category: List<Meal>)  :
-    BaseFragment<FragmentCategoriesBinding>() , CategoriesAdapter.CategoriesListener{
+class CategoriesFragment :
+    BaseFragment<FragmentCategoriesBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentCategoriesBinding =
         FragmentCategoriesBinding::inflate
 
-    private lateinit var categoryAdapter: CategoriesAdapter
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        installViews(category, this, requireContext())
+
+        val category = MealCourse.values()
+
+        installViews(category)
     }
 
-    private fun installViews(
-        categories: List<Meal>,
-        categoryListener: CategoriesAdapter.CategoriesListener,
-        context: Context
-    ) {
-        categoryAdapter = CategoriesAdapter(categories, categoryListener, context)
-        binding.recyclerCategories .adapter = categoryAdapter
+    private fun installViews(categories: Array<MealCourse>) {
+        val categoryAdapter = CategoriesAdapter(CategoriesListener { loadMealFragment(it) })
+        categoryAdapter.submitList(categories.toList())
+        binding.recyclerCategories.adapter = categoryAdapter
     }
 
-
-    override fun onClick(category: Meal) {
+    private fun loadMealFragment(mealCourse: com.cheesecake.chickenmasala.model.MealCourse) {
         val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, MealsFragment.createFragment(mealCourse))
         transaction.addToBackStack(null)
-        transaction.add(R.id.fragment_container, MealFragment()).commit()
+        transaction.commit()
     }
-
-
+    @Parcelize
+    enum class MealCourse( val CourseName: String, val imageResourceId: Int) : Parcelable {
+        SOUP("Soup", R.drawable.cooking_image), APPETIZER(
+            "Soup",
+            R.drawable.cooking_image
+        ),
+        SPICY("Soup", R.drawable.cooking_image)
+    }
 }
