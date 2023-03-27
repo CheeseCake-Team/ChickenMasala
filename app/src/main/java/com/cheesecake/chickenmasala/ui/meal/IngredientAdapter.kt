22ee536d3e511dad2579c33df9de31291ca8e704
shehab <sheshox12@gmail.com>
2023-03-28 01:21:52 +0200

    update class meal fragment, ingredient adapter and instruction adapter

--- app/src/main/java/com/cheesecake/chickenmasala/ui/meal/IngredientAdapter.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/meal/IngredientAdapter.kt
@@ -8,21 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cheesecake.chickenmasala.databinding.ItemTranslatedIngredientBinding
 
 class IngredientAdapter() :
-    ListAdapter<String, IngredientAdapter.IngredientViewHolder>(IngredientItemCallback) {
+    ListAdapter<String, IngredientAdapter.IngredientViewHolder>(IngredientDiffUtil()) {
 
-    companion object IngredientItemCallback :
-        DiffUtil.ItemCallback<String>() {
-        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem === newItem
-        }
-
-        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem == newItem
-        }
-    }
-
-
-    class IngredientViewHolder private constructor(private var binding: ItemTranslatedIngredientBinding) :
+    class IngredientViewHolder(private val binding: ItemTranslatedIngredientBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: String) {
@@ -30,21 +18,29 @@ class IngredientAdapter() :
                 textIngredient.text = item
             }
         }
-
-        companion object {
-            fun from(parent: ViewGroup): IngredientViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = ItemTranslatedIngredientBinding.inflate(layoutInflater, parent, false)
-                return IngredientViewHolder(binding)
-            }
-        }
     }
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder =
-        IngredientViewHolder.from(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemTranslatedIngredientBinding.inflate(layoutInflater, parent, false)
+        return IngredientViewHolder(binding)
+    }
 
     override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) =
         holder.bind(getItem(position))
+
 }
+
+class IngredientDiffUtil : DiffUtil.ItemCallback<String>() {
+
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+}
+
+
 
---
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/meal/InstructionsAdapter.kt
@@ -0,0 +1,48 @@
+package com.cheesecake.chickenmasala.ui.meal
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.cheesecake.chickenmasala.R
+import com.cheesecake.chickenmasala.databinding.ItemTranslatedInstructionBinding
+
+class InstructionsAdapter :
+    ListAdapter<String, InstructionsAdapter.InstructionsViewHolder>(InstructionDiffUtil()) {
+
+    class InstructionsViewHolder(private val binding: ItemTranslatedInstructionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(item: String, position: Int) {
+            binding.apply {
+                textStep.text = textStep.context.getString(R.string.stepCode, position + 1)
+                textStepContent.text = item
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InstructionsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemTranslatedInstructionBinding.inflate(inflater, parent, false)
+        return InstructionsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: InstructionsViewHolder, position: Int) {
+        holder.bind(getItem(position), position)
+    }
+}
+
+class InstructionDiffUtil : DiffUtil.ItemCallback<String>() {
+
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+}
