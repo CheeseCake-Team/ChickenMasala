7f381610923a0779b60f3f168dd670dd6c1b1f30
shehab <sheshox12@gmail.com>
2023-03-28 18:59:23 +0200

    update class csv data source and parser

--- app/src/main/java/com/cheesecake/chickenmasala/datasource/CsvDataSource.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/datasource/CsvDataSource.kt
@@ -7,17 +7,15 @@ import java.io.InputStreamReader
 
 class CsvDataSource(private val parser: CsvParser, private val fileReader: InputStream) :
     MealsDataSource {
-
     override fun getAllMealsData(): List<Meal> {
-
         val buffer = BufferedReader(InputStreamReader(fileReader))
-        buffer.readLine()
-        var line: String?
-        while (buffer.readLine().also { line = it } != null) {
-            parser.parseTotalColumns(line!!)
-        }
+        val list = buffer.readText().split(",")
         buffer.close()
 
-        return parser.getMeals()
+        val modifiedList = list.mapIndexed { index, element ->
+            if (index % 8 == 0) element.split("\n") else listOf(element)
+        }.flatten()
+
+        return modifiedList.drop(9).chunked(9).map { parser.parseMeal(it) }
     }
 }
--- app/src/main/java/com/cheesecake/chickenmasala/datasource/CsvParser.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/datasource/CsvParser.kt
@@ -4,50 +4,19 @@ import com.cheesecake.chickenmasala.model.Meal
 import com.cheesecake.chickenmasala.model.MealCourse
 
 class CsvParser {
-    private val tokenizedList: MutableList<String> = mutableListOf()
-    private val meals: MutableList<Meal> = mutableListOf()
 
-    fun parseTotalColumns(csvLine: String) {
-        if (csvLine.contains(",")) {
-            val tokenizedList1 = csvLine.split(",").toMutableList()
-            if (tokenizedList1.first().contains("\"")) {
-                appendToLastIndexInTokenizedList(tokenizedList1.first())
-                tokenizedList1.removeFirst()
-            }
-            tokenizedList.addAll(tokenizedList1)
-            if (tokenizedList.size >= ColumnIndex.TOTAL_COLUMNS) {
-                val mealTokens = tokenizedList.take(ColumnIndex.TOTAL_COLUMNS)
-                val meal = parseMeal(mealTokens)
-                meals.add(meal)
-                tokenizedList.removeAll(mealTokens)
-            }
-        } else {
-            appendToLastIndexInTokenizedList(csvLine)
-        }
-    }
-
-    private fun appendToLastIndexInTokenizedList(string: String) {
-        val lastToken = tokenizedList.last()
-        val newToken = "$lastToken $string"
-        tokenizedList[tokenizedList.lastIndex] = newToken
-    }
-
-    fun getMeals(): List<Meal> {
-        return meals.toList()
-    }
-
-    private fun parseMeal(tokenizedList: List<String>): Meal {
+    fun parseMeal(tokenizedList: List<String>): Meal {
         return Meal(
-            translatedRecipeName = tokenizedList[ColumnIndex.TRANSLATED_RECIPE_NAME],
-            url = tokenizedList[ColumnIndex.URL],
-            imageUrl = tokenizedList[ColumnIndex.IMAGE_URL],
-            cuisine = tokenizedList[ColumnIndex.CUISINE],
-            translatedInstructions = tokenizedList[ColumnIndex.TRANSLATED_INSTRUCTIONS].split("."),
-            TotalTimeInMinutes = tokenizedList[ColumnIndex.TOTAL_TIME_IN_MINUTES].toInt(),
-            cleanedIngredients = tokenizedList[ColumnIndex.CLEANED_INGREDIENTS].split("-"),
-            translatedIngredients = tokenizedList[ColumnIndex.TRANSLATED_INGREDIENTS].split("-"),
-            ingredientCount = tokenizedList[ColumnIndex.INGREDIENT_COUNT].toInt(),
-            course = getMealCourse(tokenizedList[ColumnIndex.TRANSLATED_RECIPE_NAME])
+            translatedRecipeName = tokenizedList[Constants.ColumnIndex.TRANSLATED_RECIPE_NAME],
+            url = tokenizedList[Constants.ColumnIndex.URL],
+            imageUrl = tokenizedList[Constants.ColumnIndex.IMAGE_URL],
+            cuisine = tokenizedList[Constants.ColumnIndex.CUISINE],
+            translatedInstructions = tokenizedList[Constants.ColumnIndex.TRANSLATED_INSTRUCTIONS].replace("\n", "").replace("\"", "").split(".").dropLast(1),
+            TotalTimeInMinutes = tokenizedList[Constants.ColumnIndex.TOTAL_TIME_IN_MINUTES].toInt(),
+            cleanedIngredients = tokenizedList[Constants.ColumnIndex.CLEANED_INGREDIENTS].split("-"),
+            translatedIngredients = tokenizedList[Constants.ColumnIndex.TRANSLATED_INGREDIENTS].split("-"),
+            ingredientCount = tokenizedList[Constants.ColumnIndex.INGREDIENT_COUNT].trim().toInt(),
+            course = getMealCourse(tokenizedList[Constants.ColumnIndex.TRANSLATED_RECIPE_NAME])
 
         )
     }
@@ -56,7 +25,7 @@ class CsvParser {
         return when {
             mealName.contains("Soup") -> MealCourse.SOUPS
             mealName.contains("Spicy") -> MealCourse.SPICY
-            mealName.contains("Chicken") -> MealCourse.CHICKEN
+            mealName.contains("chicken") -> MealCourse.CHICKEN
             mealName.contains("Vegetables") -> MealCourse.VEGETABLES
             mealName.contains("Masala") -> MealCourse.MASALA
             mealName.contains("Cake") -> MealCourse.CAKES
@@ -70,16 +39,5 @@ class CsvParser {
 
 
 
-    object ColumnIndex {
-        const val TRANSLATED_RECIPE_NAME = 0
-        const val TRANSLATED_INGREDIENTS = 1
-        const val TOTAL_TIME_IN_MINUTES = 2
-        const val CUISINE = 3
-        const val TRANSLATED_INSTRUCTIONS = 4
-        const val URL = 5
-        const val CLEANED_INGREDIENTS = 6
-        const val IMAGE_URL = 7
-        const val INGREDIENT_COUNT = 8
-        const val TOTAL_COLUMNS = 9
-    }
+
 }
