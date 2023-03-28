ce245d83855b1ea42679825e6d6efd039fe50fa0
shehab <sheshox12@gmail.com>
2023-03-28 19:05:04 +0200

    create class meal adapter diff util

---
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/meal/MealAdapterDiffUtil.kt
@@ -0,0 +1,15 @@
+package com.cheesecake.chickenmasala.ui.meal
+
+import androidx.recyclerview.widget.DiffUtil
+
+class MealAdapterDiffUtil: DiffUtil.ItemCallback<String>() {
+
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+}
