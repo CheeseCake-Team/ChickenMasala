c5e8e73e2c6e853c2218d63334c50a6bff01387f
hassanwasfy <wasfyelgamed@gmail.com>
2023-03-29 00:36:33 +0200

    edit categories frag adapter listener

--- app/src/main/java/com/cheesecake/chickenmasala/ui/categories/CategoriesAdapter.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/categories/CategoriesAdapter.kt
@@ -8,20 +8,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cheesecake.chickenmasala.databinding.ItemCategoryBinding
 import com.cheesecake.chickenmasala.model.MealCourse
 
-
-class CategoriesAdapter(private val clickListener: CategoriesListener) :
+class CategoriesAdapter(private val clickListener: (item: MealCourse) -> Unit) :
     ListAdapter<MealCourse, CategoriesAdapter.CategoriesViewHolder>(CategoriesItemCallback()) {
 
     class CategoriesViewHolder(private var binding: ItemCategoryBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(listener: CategoriesListener, item: MealCourse) {
+        fun bind(clickListener: (item: MealCourse) -> Unit, item: MealCourse) {
             binding.apply {
                 textViewTextAddress.text = item.courseName
                 cardImgHolder.setImageResource(item.imageResourceId)
-            }.root.setOnClickListener { listener.onClick(item) }
+                root.setOnClickListener { clickListener(item) }
+            }
         }
-
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -31,11 +31,6 @@ class CategoriesAdapter(private val clickListener: CategoriesListener) :
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) =
         holder.bind(clickListener, getItem(position))
-}
-
-
-class CategoriesListener(val clickListener: (item: MealCourse) -> Unit) {
-    fun onClick(item: MealCourse) = clickListener(item)
 }
 
 class CategoriesItemCallback :
