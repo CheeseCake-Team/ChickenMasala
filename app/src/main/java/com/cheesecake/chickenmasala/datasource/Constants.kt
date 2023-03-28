42e22dba662ae45790d5ea1c0af289eda165ba31
shehab <sheshox12@gmail.com>
2023-03-28 18:58:38 +0200

    create object constants

---
+++ app/src/main/java/com/cheesecake/chickenmasala/datasource/Constants.kt
@@ -0,0 +1,15 @@
+package com.cheesecake.chickenmasala.datasource
+
+object Constants {
+    object ColumnIndex {
+        const val TRANSLATED_RECIPE_NAME = 0
+        const val TRANSLATED_INGREDIENTS = 1
+        const val TOTAL_TIME_IN_MINUTES = 2
+        const val CUISINE = 3
+        const val TRANSLATED_INSTRUCTIONS = 4
+        const val URL = 5
+        const val CLEANED_INGREDIENTS = 6
+        const val IMAGE_URL = 7
+        const val INGREDIENT_COUNT = 8
+    }
+}
