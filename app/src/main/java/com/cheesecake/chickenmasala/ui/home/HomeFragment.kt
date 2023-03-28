52082f4b95854057db02fbef6ddf178288fcdb14
hassanwasfy <wasfyelgamed@gmail.com>
2023-03-29 00:46:52 +0200

    create AdviceFactory + move prepareFoodAdviceList() to it

--- app/src/main/java/com/cheesecake/chickenmasala/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/home/HomeFragment.kt
@@ -20,7 +20,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     private fun setupViews() {
         val adviceAdapter = AdviceImageSliderAdapter().apply {
-            submitList(prepareFoodAdviceList())
+            submitList(AdviceFactory(requireContext()).prepareFoodAdviceList)
         }
 
         val fastRecipeAdapter =
@@ -51,44 +51,5 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         }
     }
 
-    private fun prepareFoodAdviceList() = mutableListOf(
-        Advice(
-            requireContext().getString(R.string.eat_fruits_veggies_title),
-            requireContext().getString(R.string.eat_fruits_veggies_body),
-            R.drawable.fruits_image
-        ),
 
-        Advice(
-            requireContext().getString(R.string.limit_processed_foods_title),
-            requireContext().getString(R.string.limit_processed_foods_body),
-            R.drawable.food_image_six
-        ),
-
-        Advice(
-            requireContext().getString(R.string.choose_lean_protein_title),
-            requireContext().getString(R.string.choose_lean_protein_body),
-            R.drawable.food_image_thirteen
-        ),
-
-        Advice(
-            requireContext().getString(R.string.reduce_added_sugar_title),
-            requireContext().getString(R.string.reduce_added_sugar_body), R.drawable.suger_image
-        ),
-
-        Advice(
-            requireContext().getString(R.string.stay_hydrated_title),
-            requireContext().getString(R.string.stay_hydrated_body), R.drawable.water_image
-        ),
-
-        Advice(
-            requireContext().getString(R.string.choose_healthy_fats_title),
-            requireContext().getString(R.string.choose_healthy_fats_body),
-            R.drawable.food_image_ten
-        ),
-
-        Advice(
-            requireContext().getString(R.string.eat_breakfast_title),
-            requireContext().getString(R.string.eat_breakfast_body), R.drawable.food_image_nine
-        )
-    )
 }
