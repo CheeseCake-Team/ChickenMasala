814108efbdf59724b9147740aa4bfe58473736bb
Nahla <nahla@email.com>
2023-04-01 22:58:56 +0200

    add new categories

--- app/src/main/java/com/cheesecake/chickenmasala/model/MealCourse.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/model/MealCourse.kt
@@ -6,15 +6,11 @@ import com.cheesecake.chickenmasala.R
 import kotlinx.parcelize.Parcelize
 
 enum class MealCourse(val courseName: String, @DrawableRes val imageResourceId: Int)  {
-    SOUPS("soups", R.drawable.soup_category),
-    SPICY("spicy", R.drawable.spicy_category),
-    BAKED("baked", R.drawable.baked),
-    VEGETABLES("vegetables", R.drawable.vegetables_category),
-    MASALA("masala", R.drawable.masala_category),
-    BREAKFAST("breakfast", R.drawable.breakfast_category),
-    CAKES("cakes", R.drawable.cake_category),
-    GRILLED("grilled", R.drawable.food_image_thirteen),
-    BREAD("bread", R.drawable.food_image_nine),
-    RICE("rice", R.drawable.rice),
-    JUICE("juice", R.drawable.juice)
+    SOUPS("Soups", R.drawable.soup_category),
+    SPICY("Spicy", R.drawable.spicy_category),
+    CHICKEN("chicken", R.drawable.food_image_six),
+    VEGETABLES("Vegetables", R.drawable.vegetables_category),
+    MASALA("Masala", R.drawable.masala_category),
+    BREAKFAST("Breakfast", R.drawable.breakfast_category),
+    CAKES("Cakes", R.drawable.cake_category)
 }
