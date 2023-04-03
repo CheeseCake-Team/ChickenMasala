d973e8113026c2d1d9b7dcefb1df577ccb7b4686
khaled eid <khaled.eid1k1k@gmail.com>
2023-04-03 05:43:22 +0200

    apply shared preferences for fragments

--- app/src/main/java/com/cheesecake/chickenmasala/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/categories/CategoriesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.cheesecake.chickenmasala.R
 import com.cheesecake.chickenmasala.databinding.FragmentCategoriesBinding
+import com.cheesecake.chickenmasala.datasource.Constants
 import com.cheesecake.chickenmasala.model.MealCourse
 import com.cheesecake.chickenmasala.ui.base.BaseFragment
 import com.cheesecake.chickenmasala.ui.meals.MealsFragment
@@ -27,6 +28,8 @@ class CategoriesFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViews()
+        this.returnStatue(binding.recyclerCategoriesHolder,Constants.Keys.CATEGORIES)
+
     }
 
     private fun setupViews() {
@@ -43,5 +46,8 @@ class CategoriesFragment :
             commit()
         }
     }
-
+    override fun onPause() {
+        super.onPause()
+        this.saveStatue(binding.recyclerCategoriesHolder, Constants.Keys.CATEGORIES)
+    }
 }
--- app/src/main/java/com/cheesecake/chickenmasala/ui/cuisines/CuisinesFragment.kt
+++ app/src/main/java/com/cheesecake/chickenmasala/ui/cuisines/CuisinesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.cheesecake.chickenmasala.R
 import com.cheesecake.chickenmasala.databinding.FragmentCategoriesBinding
+import com.cheesecake.chickenmasala.datasource.Constants
 import com.cheesecake.chickenmasala.interactor.RecipesInteractor
 import com.cheesecake.chickenmasala.ui.base.BaseFragment
 import com.cheesecake.chickenmasala.ui.meals.MealsFragment
@@ -22,6 +23,8 @@ class CuisinesFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViews()
+        this.returnStatue(binding.recyclerCategoriesHolder,Constants.Keys.CUISINES)
+
     }
 
     override fun setActionBarTitle() {
@@ -43,6 +46,10 @@ class CuisinesFragment :
             addToBackStack(null)
             commit()
         }
+    }
+    override fun onPause() {
+        super.onPause()
+        this.saveStatue(binding.recyclerCategoriesHolder, Constants.Keys.CUISINES)
     }
 
 }
